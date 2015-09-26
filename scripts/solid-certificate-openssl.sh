4c70e1c76c9ff133ec4b570b343e3eae611ac1a7
Chris Earnhart <cearnhart@solidoptionos.com>
2015-09-26 18:48:53 -0500

    Update and rename openssl.sh to solid-certificate-openssl.sh
    good to go

--- scripts/solid-certificate-openssl.sh
+++ scripts/solid-certificate-openssl.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
-# Author Joseph Sviderski
+clear
+#Colors
 
-echo "Initializing Certifcation Authority Environment: "  
+dgrey="\033[0;34m"
+port="\033[0;33m"
+RED="\033[1;31m"
+GREEN="\033[2;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+RED="\033[0;31m"
+ENDCOLOR="\033[0m"
+
+echo -e $RED"#################################################################"$ENDCOLOR; 
+echo -e $YELLOW"   ____     ___    __  ____       __  _             ____  ____"$ENDCOLOR;
+echo -e $YELLOW"  / __/__  / (_)__/ / / __ \___  / /_(_)__  ___    / __ \/ __/"$ENDCOLOR;
+echo -e $YELLOW" _\ \/ _ \/ / / _  / / /_/ / _ \/ __/ / _ \/ _ \  / /_/ /\ \  "$ENDCOLOR;
+echo -e $YELLOW"/___/\___/_/_/\_,_/  \____/ .__/\__/_/\___/_//_/  \____/___/  "$ENDCOLOR;
+echo -e $YELLOW"                         /_/                                  "$ENDCOLOR;
+echo -e $RED"#################################################################"$ENDCOLOR;
+echo -e $YELLOW"Setup OpenSSL Certificate"$ENDCOLOR;
+
+echo -e $BLUE"Initializing Certifcation Authority Environment "$ENDCOLOR; 
+sleep 0.5
 cd && mkdir -pv myCA/ 
-echo "Initializing Certificate Database: "
+echo -e $BLUE "Initializing Certificate Database "$ENDCOLOR; 
+sleep 0.5
 echo '01' > serial  && touch index.txt
-echo "Initializing CA Configuration"
+echo -e $BLUE "Initializing CA Configuration"
 
 echo "[ ca ]
 default_ca      = local_ca
@@ -49,13 +70,16 @@ organizationalUnitName  = IT Department
 basicConstraints        = CA:true" >> myCA/caconfig.cnf
 
 export OPENSSL_CONF=myCA/caconfig.cnf 
-echo "Generating CA Certificate and Key: "
+echo -e $BLUE "Generating CA Certificate and Key "$ENDCOLOR; 
+sleep 0.5
 openssl req -x509 -newkey rsa:2048 -out cacert.pem -outform PEM -days 1825
  
-echo "Strip the Certificate of Text -CERTIFCATE- Generating .crt: "
+echo -e $BLUE "Strip the Certificate of Text -CERTIFCATE- Generating .crt "$ENDCOLOR; 
+sleep 0.5
 openssl x509 -in cacert.pem -out cacert.crt
 
-echo "Generating a Self-Signed Certificate: "
+echo -e $BLUE "Generating a Self-Signed Certificate "$ENDCOLOR; 
+sleep 0.5
 
 echo " 
 [ req ]
@@ -78,47 +102,70 @@ DNS.0                   = tradeshowhell.com
 DNS.1                   = alt.tradeshowhell.com" >> myCA/signed.cnf
 
 export OPENSSL_CONF=myCA/signed.cnf 
-echo "Generating the Server Certificate and Key: "
+echo -e $BLUE "Generating the Server Certificate and Key "$ENDCOLOR; 
+sleep 0.5
 openssl req -newkey rsa:1024 -keyout tempkey.pem -keyform PEM -out tempreq.pem -outform PEM
 
-echo "Translate Temporary Key to Unencrypted Key"
+echo -e $BLUE "Translate Temporary Key to Unencrypted Key"$ENDCOLOR; 
+sleep 0.5
 openssl rsa < tempkey.pem > server_key.pem
 
-echo "Signing Server Certificate with the CA key"
+echo -e $BLUE "Signing Server Certificate with the CA key"$ENDCOLOR; 
+sleep 0.5
 openssl ca -in tempreq.pem -out server_crt.pem
 
-echo "Cleanup Removing All Temporary Certificates and Keys: "
+echo -e $BLUE "Cleanup Removing All Temporary Certificates and Keys... "$ENDCOLOR; 
+sleep 0.5
 rm -f tempkey.pem && rm -f tempreq.pem
 
-echo "Prepending Unencrypted Private Key to the Server Certificate: "
+echo -e $BLUE "Prepending Unencrypted Private Key to the Server Certificate... "$ENDCOLOR; 
+sleep 0.5
 
 cat server_key.pem server_crt.pem > hold.pem
 mv hold.pem server_crt.pem
 chmod 400 server_crt.pem
 
-echo "Converting X.509 Certificates to PKCS#12 for Client Application Certificate: "
+echo -e $BLUE "Converting X.509 Certificates to PKCS#12 for Client Application Certificate... "$ENDCOLOR; 
+sleep 0.5
 
 openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out mycert.pem
 
-echo "Compiling Certificate and Private Key: "
+echo -e $BLUE "Compiling Certificate and Private Key... "$ENDCOLOR; 
+sleep 0.5
 openssl pkcs12 -export -out mycert.pfx -in mycert.pem -name "Application"
 
-echo "Generating Certificate and Private key Request: "
+echo -e $BLUE "Generating Certificate and Private key Request... "$ENDCOLOR; 
+sleep 0.5
 copenssl req -new -newkey rsa:1024 -nodes -keyout mykey.pem -out myreq.pem
 
-echo "Signature Verification: "
+echo -e $BLUE "Signature Verification: "$ENDCOLOR; 
+sleep 0.5
 penssl req -in myreq.pem -noout -verify -key mykey.pem
 
-echo "Information Verification: "
+echo -e $BLUE "Information Verification: "$ENDCOLOR; 
+sleep 0.5
 openssl req -in myreq.pem -noout -text
 
-echo "Deploy Certificate to the System Certificate Directory"
+echo -e $BLUE "Deploying Certificate to the System Certificate Directory..."$ENDCOLOR; 
+sleep 0.5
 sudo cp mycert.pem /usr/share/ca-certificates/mycert.crt
 
-echo "Appending CA-Certificates Configuration: "
+echo -e $BLUE "Appending CA-Certificates Configuration... "$ENDCOLOR; 
+sleep 0.5
 sudo dpkg-reconfigure ca-certificates
 
-echo "Updating CA Certificates Database: "
+echo -e $BLUE "Updating CA Certificates Database... "$ENDCOLOR; 
+sleep 0.5
 sudo update-ca-certificates
 
-echo "Completed Successfully Imported Certificate into the System CA Certificates Database: "
+echo -e $BLUE " "$ENDCOLOR; 
+sleep 0.5
+echo ""
+echo ""
+echo -e $RED"##########################################################################################"$ENDCOLOR
+echo -e $RED"#  Complete. Successfully Imported Certificate into the System CA Certificates Database  #"$ENDCOLOR
+echo -e $RED"##########################################################################################"$ENDCOLOR
+echo ""
+echo -e $GREEN"Have A Solid Day"$ENDCOLOR
+echo ""
+exit
