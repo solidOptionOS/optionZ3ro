038569eecbd722172a0300f5926b4c1c46810b7a
Chris Earnhart <cearnhart@solidoptionos.com>
2015-09-26 19:36:17 -0500

    Update solid-cleaner.sh

--- scripts/solid-cleaner.sh
+++ scripts/solid-cleaner.sh
@@ -23,25 +23,29 @@ echo -e $GREEN"Thanks and credit to the team at blackhatsec.org/"$ENDCOLOR;
 echo -e $GREEN"for creating CBS Cleaner - the basis for this script"$ENDCOLOR;
 echo -e $GREEN"This modificatin of CBS Cleaner by h3x at solidoptionos.com"$ENDCOLOR;
 echo -e $RED"#################################################################"$ENDCOLOR;
-echo Starting Cleaning Function | festival --tts 
- 
+echo ""
+echo -e $BLUE"Checking for root..."$ENDCOLOR;
+sleep 0.5
 if [ $USER != root ]; then
 echo -e $RED"Sorry, you must run this from as root."$ENDCOLOR
 echo -e $YELLOW"Exiting..."$ENDCOLOR
 exit 0
 fi
-
+echo -e $BLUE"Installing festival first..."$ENDCOLOR;
+sleep 1
+apt-get install festival -Y
 echo ""
+echo -e $BLUE"Ok. Let's clean."$ENDCOLOR;
 echo ""
-apt-get install festival
-sleep 0.5
-echo -e $YELLOW"Cleaning var/temp..."$ENDCOLOR
+echo Starting Cleaning Function | festival --tts 
+echo ""
+echo -e $BLUE"Cleaning var/temp..."$ENDCOLOR;
 sleep 0.5
 cd /var/tmp/
 sleep 0.5
 rm -rf -i /var/tmp/
 sleep 0.5
-echo -e $BLUE"Would you like to attempt to repair/fix any broken packages? y/n"$ENDCOLOR
+echo -e $YELLOW"Would you like to attempt to repair/fix any broken packages? y/n"$ENDCOLOR;
 read fix
 if [[ $fix = Y || $fix = y ]] ; then
 for package in $(apt-get upgrade 2>&1 | grep "warning: files list file" | sed "s/.*'//; s/://"); do apt-get install --reinstall "$package"; done
@@ -52,58 +56,58 @@ echo""
 fi
 echo ""
 echo ""
-echo -e $YELLOW"Cleaning /var/log/..."$ENDCOLOR
+echo -e $BLUE"Cleaning /var/log/..."$ENDCOLOR
 rm -rf -i -v /var/log/
 rm -i -v /var/cache/* --force
 
 echo "" 
-echo -e $YELLOW"Cleaning /var/cache/..."$ENDCOLOR
+echo -e $BLUE"Cleaning /var/cache/..."$ENDCOLOR
 sudo rm -v /var/cache/apt/archives/*.deb  
 sleep 0.5
 sudo rm -v /var/log/*.gz  -f
 sleep 0.5
 sudo rm -rf ~/root/.local/share/Trash/*
 sleep 0.5
-echo -e $YELLOW"Cleaning old kernels..."$ENDCOLOR
+echo -e $BLUE"Cleaning old kernels..."$ENDCOLOR
 sudo aptitude purge $OLDKERNELS
 sleep 0.5
 echo ""
-echo -e $YELLOW"Fixing broken packages..."$ENDCOLOR
+echo -e $BLUE"Fixing broken packages..."$ENDCOLOR
 sudo apt-get check ; sudo apt-get -f install
 sleep 0.5
-echo -e $YELLOW"Cleaning trash archives..."$ENDCOLOR
+echo -e $BLUE"Cleaning trash archives..."$ENDCOLOR
 sleep 0.5
 rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
 sleep 0.5
 rm -rf /root/.local/share/Trash/*/** &> /dev/null
 sleep 0.5
-echo -e $YELLOW"[Aptitude]Cleaning..."$ENDCOLOR
+echo -e $BLUE"[Aptitude]Cleaning..."$ENDCOLOR
 sudo aptitude clean
 sleep 0.5
-echo -e $YELLOW"[Apt-Get]Removing obsolete packages and cleaning..."$ENDCOLOR
+echo -e $BLUE"[Apt-Get]Removing obsolete packages and cleaning..."$ENDCOLOR
 sudo apt-get autoremove && sudo apt-get autoclean && sudo apt-get purge && sudo apt-get clean
 sleep 0.5
 echo ""
 echo ""
-echo -e $YELLOW"[Aptitude]:Cleaning leftover configs..."$ENDCOLOR 
+echo -e $BLUE"[Aptitude]:Cleaning leftover configs..."$ENDCOLOR 
 sudo aptitude purge $OLDCONF
 dpkg -l | grep '^rc' | awk '{print $2}' | sudo xargs dpkg --purge
 sleep 0.5
 echo ""
 echo ""
-echo -e $YELLOW"[Aptitude]:Cleaning apt cache again..."$ENDCOLOR
+echo -e $BLUE"[Aptitude]:Cleaning apt cache again..."$ENDCOLOR
 sudo aptitude clean
 sleep 0.5
 echo ""
 echo ""
-echo -e $YELLOW"[Aptitude]:Removing old config files again..."$ENDCOLOR
+echo -e $BLUE"[Aptitude]:Removing old config files again..."$ENDCOLOR
 sudo aptitude purge $OLDCONF
 sleep 0.5
 apt-get autoremove
 sleep 0.5
 apt-get clean
 sleep 0.5
-echo -e $YELLOW"Emptying  trashes..."$ENDCOLOR
+echo -e $BLUE"Emptying  trashes..."$ENDCOLOR
 
 echo ""
 echo ""
@@ -112,10 +116,10 @@ rm -rf /root/.local/share/Trash/*/** &> /dev/null
 echo ""
 echo ""
 echo -e $RED"###############################################"$ENDCOLOR
-echo -e $RED"#              task complete!!                #"$ENDCOLOR
+echo -e $BLUE"#          Complete. System Clean             #"$ENDCOLOR
 echo -e $RED"###############################################"$ENDCOLOR
 echo ""
-echo "Have A Solid Day"
+echo -e $YELLOW"Have A Solid Day"$ENDCOLOR;
 echo ""
 exit
 
