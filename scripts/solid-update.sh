5a77b5f81b2f90cdd9dc00ec9e866e92df49dfee
solidOptionOS <cearnhart@solidoptionos.com>
2015-10-27 11:08:51 +0000

    cleaned up update

--- scripts/solid-update.sh
+++ scripts/solid-update.sh
@@ -20,31 +20,32 @@ echo -e $RED"#################################################################"$
 echo -e $YELLOW"Solid-Update"$ENDCOLOR;
 echo ""
 echo -e $BLUE"Cleaning and purging first "$ENDCOLOR;
-sudo apt-get clean && sudo apt-get autoclean
+apt-get autoremove && apt-get clean && apt-get autoclean
 sleep 0.1
-sudo rm -rf -i /var/cache/apt/archive/
+rm -rf /var/cache/apt/archives/
 sleep 0.1
 echo ""
 echo -e $BLUE"Updating Package Cache "$ENDCOLOR;
 sleep 0.5
-sudo apt-get update && sudo aptitude update 
+apt-get update && aptitude update 
 sleep 0.5
-echo ""
-echo -e $BLUE"Installing apt-file "$ENDCOLOR;
-sleep 0.1
-sudo apt-get install apt-file -y
-sleep 0.5
+#echo ""
+#echo -e $BLUE"Installing apt-file "$ENDCOLOR;
+#sleep 0.1
+#apt-get install apt-file -y
+#sleep 0.5
 echo -e $BLUE"Updaring apt-file "$ENDCOLOR;
 echo -e $BLUE"This may take a while depending on your system "$ENDCOLOR;
 sleep 0.1
-sudo apt-file update
+apt-file update
 sleep 0.5
 echo ""
-echo -ei $BLUE"Upgrading your packages "$ENDCOLOR;
+echo -e $BLUE"Upgrading your packages "$ENDCOLOR;
 sleep 0.1
-sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo aptitude upgrade -y && sudo aptitude dist-upgrade -y
+apt-get upgrade -y sudo apt-get dist-upgrade -y && aptitude upgrade -y aptitude dist-upgrade -y
 sleep 0.5
-sudo apt-get autoremove $$ sudo apt-get autoclean
+echo -e $BLUE"Removing old packages "$ENDCOLOR;
+apt-get autoremove $$ apt-get autoclean
 sleep 0.5
 echo ""
 echo -e $YELLOW"Would you like to try and repair any broken packages? {y/n} "$ENDCOLOR;
@@ -53,23 +54,23 @@ if [[ $fixornot = 'y' || $fixornot = 'Y' ]] ; then
 echo ""
 echo -e $BLUE" OK. Attempting to repair broken packages "$ENDCOLOR;
 sleep 0.1
-sudo apt-get update --fix-missing && sudo apt-get upgrade --fix-broken -y
+apt-get update --fix-missing && apt-get upgrade --fix-broken -y
 sleep 0.5
 echo ""
-sudo apt-get -f install && sudo aptitude -f install 
+apt-get -f install && aptitude -f install 
 sleep 0.5
 echo ""
 else 
 echo -e $BLUE"Updating database, initramfs, and grub "$ENDCOLOR;
 sleep 0.1
 echo ""
-sudo updatedb
-skeep 0.5
-echo ""
-sudo update-initramfs -uv
+updatedb
 sleep 0.5
 echo ""
-sudo update-grub && sudo update-grub2
+update-initramfs -uv
+sleep 0.5
+echo ""
+update-grub && update-grub2
 sleep 0.5
 echo ""
 echo -e $BLUE"#################################"$ENDCOLOR;
@@ -88,19 +89,19 @@ echo -e $BLUE"Attempting fix with --full-resolver "$ENDCOLOR;
 echo -e $BLUE"This may take a while... seriously..."$ENDCOLOR;
 sleep 0.1
 echo ""
-sudo aptititude --full-resolver 
+aptititude install --full-resolver 
 sleep 0.5
 echo ""
 echo -e $BLUE"Updating database, initramfs, and grub "$ENDCOLOR;
 sleep 0.1
 echo ""
-sudo updatedb
-skeep 0.5
-echo ""
-sudo update-initramfs -uv
+updatedb
 sleep 0.5
 echo ""
-sudo update-grub && sudo update-grub2
+update-initramfs -uv
+sleep 0.5
+echo ""
+update-grub && update-grub2
 sleep 0.5
 echo ""
 echo -e $BLUE"#################################"$ENDCOLOR;
@@ -110,14 +111,15 @@ echo ""
 exit
 
 else
+
 echo ""
 sudo updatedb
-skeep 0.5
-echo ""
-sudo update-initramfs -uv
 sleep 0.5
 echo ""
-sudo update-grub && sudo update-grub2
+update-initramfs -uv
+sleep 0.5
+echo ""
+update-grub && update-grub2
 sleep 0.5
 echo ""
 echo -e $BLUE"#################################"$ENDCOLOR;
