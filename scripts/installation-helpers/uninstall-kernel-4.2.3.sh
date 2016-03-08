77fd08eaf6d52bdbb8c9c01487237b3b424de642
solidOptionOS <cearnhart@github.com>
2016-03-08 01:31:22 -0600

    Added support for whonix gateway and tor configuration.

--- scripts/installation-helpers/uninstall-kernel-4.2.3.sh
+++ scripts/installation-helpers/uninstall-kernel-4.2.3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Do you want to uninstall linux kernel 4.2.3?: [y/n]"
 read UNINSTALLKERNEL
-if [[ $UNINSTALLKERNEL = 'y' || $UNINSTALLKERNEL = 'Y' ]] ; then
+#if [[ $UNINSTALLKERNEL = 'y' || $UNINSTALLKERNEL = 'Y' ]] ; then
 sudo apt-get remove 'linux-headers-4.2.3*' 'linux-image-4.2.3*'
 sleep 1
 echo ""
