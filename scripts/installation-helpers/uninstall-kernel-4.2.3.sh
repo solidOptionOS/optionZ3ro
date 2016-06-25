75bda6ed218e056e00a4aae180d5df6bbf2664c7
solidOptionOS <cearnhart@solidoptionos.com>
2016-06-25 04:45:43 -0500

    updating scripts

--- scripts/installation-helpers/uninstall-kernel-4.2.3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Do you want to uninstall linux kernel 4.2.3?: [y/n]"
-read UNINSTALLKERNEL
-#if [[ $UNINSTALLKERNEL = 'y' || $UNINSTALLKERNEL = 'Y' ]] ; then
-sudo apt-get remove 'linux-headers-4.2.3*' 'linux-image-4.2.3*'
-sleep 1
-echo ""
-echo "Operation Complete."
